2cfbbb9ef890d1219d70f84a162b4e0b3024785a
Lahiry <raphaellcmb@al.insper.edu.br>
2020-04-01 23:36:36 -0300

    Perguntando se o jogador quer ver as regras

--- craps.py
+++ craps.py
@@ -9,3 +9,14 @@ time.sleep(1)
 ready = input('Se quiser jogar digite "sim", se não, digite "não": ')
 while ready != 'sim' and ready != 'não':
     ready = input('Se quiser jogar digite "sim", se não, digite "não": ')
+
+# Perguntando se o jogador quer ver as regras    
+if ready == 'sim':
+    print('Gostaria de ver as regras?')
+    time.sleep(1)
+    regras = input('Digite "sim" para ver as regras ou "não" para começar a jogar: ')
+    while regras != 'sim' and regras != 'não':
+        regras = input('Digite "sim" para ver as regras ou "não" para começar a jogar: ')
+if ready == 'não':
+    print('Obrigado pela sua visita! Volte sempre!')
+    sys.exit()
