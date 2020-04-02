03e160a13af05e27f9b6b860eeda2b55bc16779b
Lahiry <raphaellcmb@al.insper.edu.br>
2020-04-01 23:42:44 -0300

    Mostrando as regras

--- craps.py
+++ craps.py
@@ -20,3 +20,53 @@ if ready == 'sim':
 if ready == 'não':
     print('Obrigado pela sua visita! Volte sempre!')
     sys.exit()
+
+# Mostrando as regras do Craps caso o usuário queira
+if regras == 'sim':
+    print("""\
+    
+Regras do Craps:
+    O jogo acontece por meio de rodadas sucessivas, onde você deve decidir 
+    apostar ou sair do jogo, ou automaticamente sair se acabaram todas suas fichas. 
+    Neste jogo uma rodada pode ter duas fases, começando com uma chamada de “Come Out” 
+    e conforme o resultado, passar para a fase de “Point”. Você pode fazer vários tipos 
+    de apostas conforme a fase. O lançamento de dois dados de 6 lados é feito de modo 
+    aleatório para o jogo. Conforme a fase do jogo, você terá diferentes opções de apostas
+    e poderá fazer apostas de mais de um tipo por vez.
+
+
+    Os tipo de apostas são as seguintes:
+
+
+    Pass Line Bet – Esta aposta só pode ser feita na fase de “Come Out”. Se a soma dos
+    dados lançados for 7 ou 11, você ganha (por exemplo: se apostou 10 fichas, mantem
+    as 10 e recebe mais 10). Já se os dados somarem 2, 3 ou 12, (chamado de craps) você
+    perde (ou seja, se apostou 10 fichas, não recebe nada e perde essas 10). Já se a soma
+    dos dados der 4, 5, 6, 8, 9 ou 10, o jogo muda para a fase de “Point” e o objetivo muda. 
+    A aposta já feita continua valendo, porém, o valor tirado se torna o Point e para você
+    ganhar agora, a soma do novo lançamento dos dados deve ser igual ao do Point. Se a
+    nova soma dos dados é a mesma do que foi guardado no Point, você ganha o mesmo
+    valor que apostou. Se sair uma soma de valor 7, você perde tudo. Caso saia qualquer
+    outro número, se mantem na fase de “Point” sem perder ou ganhar, e continua o lançamento 
+    de dados até um veredito, quando sair ou o número do Point, ou o 7, terminando essa
+    rodada e deixando começar uma nova em “Come Out”.
+
+
+    Field – Esta aposta pode ser feita em qualquer fase do jogo. Nesta aposta se 
+    os dados derem 5, 6, 7 ou 8, você perde tudo, já se derem 3, 4, 9, 10, ou 11, 
+    você ganha o mesmo valor que apostou. Já se a soma for 2, você ganha o dobro do
+    que apostou (se apostou 10 fichas, fica com as 10 e ganha mais 20). Finalmente se 
+    sai 12 nos dados você ganha o triplo (se apostou 10 fichas, fica com as 10 e ganha mais 30).
+
+
+    Any Craps – Esta aposta pode ser feita em qualquer fase do jogo. Nesta aposta se o
+    dados derem 2, 3 ou 12, você ganha sete vezes o que apostou, caso contrário perde a aposta.
+
+
+    Twelve – Esta aposta pode ser feita em qualquer fase do jogo. Nesta aposta se o dados
+    derem 12, você ganha trinta vezes o que apostou, ou perde a aposta.
+
+""")
+    pronto = input('Digite "pronto" quando quiser começar: ')
+    while pronto != "pronto":
+        pronto = input('Digite "pronto" quando quiser começar: ')
